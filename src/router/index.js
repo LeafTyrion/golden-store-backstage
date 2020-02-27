2eda8f527744d9763720f84e2ab340c0440b6945
Leaf <yyq920201895@163.com>
2020-02-27 19:56:29 +0800

    done login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from "../components/Login";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    }
 ];
 
 const router = new VueRouter({
-  routes
+    routes
 });
 
 export default router

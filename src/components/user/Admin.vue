7b331f9365f2783cd5e7b1d534feee5b55b3f9ad
Leaf <yyq920201895@163.com>
2020-05-29 16:30:04 +0800

    done the admin

--- src/components/user/Admin.vue
+++ src/components/user/Admin.vue
@@ -9,36 +9,29 @@
         <!--卡片视图区域-->
         <el-card class="box-card">
             <el-row :gutter="20">
+                <!--搜索区域-->
+                <el-col :span="7">
+                    <el-input placeholder="请输入内容" v-model="queryInfo.query">
+                        <el-button slot="append" icon="el-icon-search" @click="getAdminList"></el-button>
+                    </el-input>
+                </el-col>
                 <!--按钮区域-->
                 <el-col :span="4">
                     <el-button type="primary" @click="addDialogVisible=true">添加管理员</el-button>
                 </el-col>
             </el-row>
             <!--列表区域-->
-            <el-table :data="userList" border stripe>
+            <el-table :data="adminList" border stripe>
                 <el-table-column type="index" label="#"/>
                 <el-table-column label="姓名" prop="name"/>
-                <el-table-column label="昵称" prop="nickName"/>
-                <el-table-column label="地址" prop="address"/>
-                <el-table-column label="手机号" prop="telephone"/>
-                <el-table-column label="状态" width="90px" prop="status">
-                    <template slot-scope="scope">
-                        <el-switch v-model="scope.row.status" @change="userStateChanged(scope.row)"/>
-                    </template>
-                </el-table-column>
+                <el-table-column label="用户名" prop="username"/>
+                <el-table-column label="密码" prop="password"/>
                 <!--操作区域-->
                 <el-table-column label="操作" width="200px">
                     <template slot-scope="scope">
-                        <el-tooltip effect="dark" content="编辑" placement="top" :enterable="false">
-                            <el-button @click="showEditDialog(scope.row.name)" type="primary" icon="el-icon-edit"
-                                       size="mini"/>
-                        </el-tooltip>
                         <el-tooltip effect="dark" content="删除" placement="top" :enterable="false">
-                            <el-button @click="removeAdminById(scope.row.name)" type="danger" icon="el-icon-delete"
+                            <el-button @click="removeAdminById(scope.row)" type="danger" icon="el-icon-delete"
                                        size="mini"/>
-                        </el-tooltip>
-                        <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
-                            <el-button type="warning" icon="el-icon-setting" size="mini"/>
                         </el-tooltip>
                     </template>
                 </el-table-column>
@@ -46,9 +39,9 @@
             <!--分页区域-->
             <el-pagination @size-change="handleSizeChange"
                            @current-change="handleCurrentChange"
-                           :current-page="queryInfo.pagenum"
-                           :page-sizes="[1,2,5,10]"
-                           :page-size="queryInfo.pagesize"
+                           :current-page="queryInfo.page"
+                           :page-sizes="[5,10,15,20]"
+                           :page-size="queryInfo.size"
                            layout="total,sizes,prev,pager,next,jumper"
                            :total="total"/>
         </el-card>
@@ -71,39 +64,9 @@
                 <el-form-item label="确认密码" prop="confirmPwd">
                     <el-input type="password" v-model="addForm.confirmPwd"></el-input>
                 </el-form-item>
-                <el-form-item label="手机号" prop="telephone">
-                    <el-input v-model="addForm.telephone"></el-input>
-                </el-form-item>
             </el-form>
             <span slot="footer" class="dialog-footer">
-                <el-button @click="dialogClosed">取 消</el-button>
-                <el-button type="primary" @click="addAdmin">确 定</el-button>
-            </span>
-        </el-dialog>
-        <el-dialog
-                title="修改管理员信息"
-                :visible.sync="editDialogVisible"
-                width="50%">
-            <!--主体内容区域-->
-            <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="90px">
-                <el-form-item label="姓名" prop="name">
-                    <el-input v-model="addForm.name"></el-input>
-                </el-form-item>
-                <el-form-item label="用户名" prop="username">
-                    <el-input v-model="addForm.username"></el-input>
-                </el-form-item>
-                <el-form-item label="密码" prop="password">
-                    <el-input type="password" v-model="addForm.password"></el-input>
-                </el-form-item>
-                <el-form-item label="确认密码" prop="confirmPwd">
-                    <el-input type="password" v-model="addForm.confirmPwd"></el-input>
-                </el-form-item>
-                <el-form-item label="手机号" prop="telephone">
-                    <el-input v-model="addForm.telephone"></el-input>
-                </el-form-item>
-            </el-form>
-            <span slot="footer" class="dialog-footer">
-                <el-button @click="dialogClosed">取 消</el-button>
+                <el-button @click="addDialogClosed">取 消</el-button>
                 <el-button type="primary" @click="addAdmin">确 定</el-button>
             </span>
         </el-dialog>
@@ -135,22 +98,20 @@
             return {
                 //请求后台的参数
                 queryInfo: {
+                    page: 1,
+                    size: 10,
                     query: '',
-                    pagenum: 1,
-                    pagesize: 2
                 },
-                userList: [],
+                adminList: [],
                 total: 0,
                 // 控制对话框打开关闭
                 addDialogVisible: false,
-                editDialogVisible: false,
                 // 表单数据
                 addForm: {
                     name: '',
                     username: '',
                     password: '',
                     confirmPwd: '',
-                    telephone: '',
                 },
                 // 表单数据验证规则
                 addFormRules: {
@@ -180,47 +141,34 @@
 
         },
         created() {
-            this.getUserList();
+            this.getAdminList();
         },
         methods: {
-            async getUserList() {
-                const result = await this.$http.get("http://127.0.0.1:8083/user/allUser", {params: this.queryInfo});
+            async getAdminList() {
+                const result = await this.$http.get("http://127.0.0.1:8081/admin/allAdmin", {params: this.queryInfo});
                 console.log(result);
                 if (result.status !== 200) {
                     return this.$message.error("获取用户列表失败")
                 }
-                this.userList = result.data.userList;
-                this.total = result.data.total;
+                this.adminList = result.data.content;
+                this.total = result.data.totalElements;
             },
             // 监听pagesize每页显示的条数改变事件
             handleSizeChange(newSize) {
                 console.log(newSize);
-                this.queryInfo.pagesize = newSize;
-                this.getUserList();
+                this.queryInfo.size = newSize;
+                this.getAdminList();
             },
             // 监听页码值改变事件
             handleCurrentChange(newPage) {
                 console.log(newPage);
-                this.queryInfo.pagenum = newPage;
-                this.getUserList();
+                this.queryInfo.page = newPage;
+                this.getAdminList();
 
             },
-            // 监听状态开关的改变
-            async userStateChanged(userinfo) {
-                console.log(userinfo);
-                const id = 1;
-                const result = await this.$http.get("http://127.0.0.1:8084/goods/updateIsSell",
-                    {params: {id: id, isSell: userinfo.status}});
-                console.log(result);
-                //todo 调用接口改变状态
-                // const result = await this.$http.put("url地址 userinfo.status")
-                // if (result.status !== 200) {
-                //     userinfo.status = !userinfo.status;
-                //     return this.$message.error("更新用户状态失败")
-                // }
-            },
+
             //对话框点击取消
-            dialogClosed() {
+            addDialogClosed() {
                 // 重置内容
                 this.$refs.addFormRef.resetFields();
 
@@ -232,30 +180,25 @@
                 this.$refs.addFormRef.validate(async valid => {
                     if (!valid) return;
 
-                    const result = await this.$http.post("http://127.0.0.1:8083/user/addUser", this.addForm);
+                    const result = await this.$http.post("http://127.0.0.1:8081/admin/addAdmin", this.addForm);
                     console.log(result);
-                    if (result.data.message !== "success") {
+                    if (result.status !== 200) {
                         this.$message.error("添加用户失败");
                         return;
                     }
                     this.$message.success("添加用户成功");
                     this.$refs.addFormRef.resetFields();
                     this.addDialogVisible = false;
-                    this.getUserList();
+                    await this.getAdminList();
                 });
 
 
             },
-            //todo 添加修改操作
-            showEditDialog(username) {
-                this.editDialogVisible = true;
-                console.log(username)
-            },
-            //todo 添加删除操作
-            async removeAdminById(name) {
+
+            // 添加删除操作
+            async removeAdminById(row) {
                 // 弹框再次确认
-                const result = await this.$confirm('是否确认删除此管理员？', '提示',
-                    {
+                const result = await this.$confirm('是否确认删除此管理员？', '提示', {
                         confirmButtonText: '确认',
                         cancelButtonText: '取消',
                         type: 'warning'
@@ -263,13 +206,14 @@
                 ).catch(error => {
                     return error;
                 });
-                console.log(result)
-
-                if (result === 'cancel') {
-                    //取消删除
-                } else {
-                    // 确认删除，调用删除接口
+                if (result !== 'confirm')
+                    return this.$message.info("取消删除");
+                const {data} = await this.$http.get("http://127.0.0.1:8081/admin/deleteAdmin", {params: {id: row.id}});
+                if (data === true) {
+                    await this.getAdminList();
+                    return this.$message.success("删除成功");
                 }
+                return this.$message.error("删除失败");
             }
         }
 
